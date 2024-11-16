028441039d3375c66804c310995b360c76db846d
mekala sai <86012659+Mekalasai77@users.noreply.github.com>
2024-11-16 19:05:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navigation from './components/Navigation';
+function App() {
+  return (
+    <>
+    <Navigation></Navigation>
+   <h1>Mekala sai's personal site</h1>
+   </>
+  );
+}
+
+export default App;
